e67e423617dac0c8b76b5c214e079a689c5ca5b1
Heyang Liu <lhypds@gmail.com>
2025-07-30 04:29:51 +0900

    Continously call awake

--- awaker.py
+++ awaker.py
@@ -11,7 +11,6 @@ load_dotenv()
 # Prevent the system from sleeping
 ES_CONTINUOUS = 0x80000000
 ES_SYSTEM_REQUIRED = 0x00000001
-ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
 
 interval = int(os.getenv("INTERVAL", 1))
 
@@ -19,6 +18,10 @@ interval = int(os.getenv("INTERVAL", 1))
 print("Press Ctrl+C to stop the awaker.")
 try:
     while True:
+        # Continuously reset the execution state to prevent sleep
+        ctypes.windll.kernel32.SetThreadExecutionState(
+            ES_CONTINUOUS | ES_SYSTEM_REQUIRED
+        )
         print("wakeup...")
         time.sleep(interval)  # Wait for x seconds
 finally:
